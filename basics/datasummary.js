e48659b6e774f5ff6829986e47141591387a3619
Vasudha <89298030+VasudhaShivane@users.noreply.github.com>
2024-12-11 18:17:27 +0000

    --

--- basics/datasummary.js
+++ basics/datasummary.js
@@ -6,3 +6,15 @@
 //Non primitive or reference
 
 //array , objects , functions
+
+
+const heros = ["shaktiman" , 'doga'];
+let myobj = {
+    name : "vgs",
+    age : 21,
+}
+
+const myfunction = function()
+{
+    console.log("hello");
+}
