f1f8a4589333bed157adf7cd150d8eb61c627d38
Vasudha <89298030+VasudhaShivane@users.noreply.github.com>
2024-12-12 06:31:39 +0000

    str

---
+++ basics/str.js
@@ -0,0 +1,14 @@
+const name = "vasudha"
+const repocount  = 30
+
+console.log(name + repocount)
+console.log(`hello my name is ${name} and my repo count is ${repocount}`);
+
+const gamename = new String('yessssss');
+console.log(gamename[2]);
+console.log(gamename.__proto__);
+console.log(gamename.length);
+console.log(gamename.toUpperCase())
+console.log(gamename.charAt(2));
+console.log(gamename.indexOf('s'));
+
