5e3ac1fc3b5fa0804b7ecefe1795316f56b043aa
Vasudha <89298030+VasudhaShivane@users.noreply.github.com>
2024-12-12 06:45:32 +0000

    maths

---
+++ basics/nums_and_maths.js
@@ -0,0 +1,19 @@
+const scor = 300
+
+console.log(scor);
+const balance = new Number(200000);
+// console.log(balance);
+const hundreds = 100000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+console.log(Math);
+console.log(Math.abs(4));
+
+console.log(Math.round(4.6));
+console.log(Math.floor(4.6));
+console.log(Math.min(3 , 7, 10, 17));
+
+console.log(Math.random());
